72b420359966e75bd65df2f668af2a48c94c98b1
CornelisPuchowicz <cpuchowicz@gmail.com>
2022-12-22 14:07:00 +0100

    Added HTML form and basic functionalities.

---
+++ src/main/java/pl/kornel/peoplewebdb/PeoplewebdbApplication.java
@@ -0,0 +1,12 @@
+package pl.kornel.peoplewebdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplewebdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeoplewebdbApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/kornel/peoplewebdb/PeoplewebdbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kornel.peoplewebdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplewebdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
